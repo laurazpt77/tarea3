f3cd1e9fb6e11f820f0e0b2f5f4f960221834472
laurazpt77 <47614964+laurazpt77@users.noreply.github.com>
2019-05-04 07:49:45 -0500

    main.cpp

---
+++ tarea3.cpp
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include<iostream>
+#include "tarea.hpp"
+
+using namespace std;
+
+ 
+int main()
+{
+   
+   int num,arreglo[20];
+   
+    srand(time(NULL));
+    
+    for(int i = 0; i<20; i++)
+    
+    {
+        num  =  rand() % (501);
+        cout<<num<<" ";
+        arreglo[i]=num;    
+    }
+    
+    cout<<"Trama de n�meros aleatorios"<<endl;
+    
+   cipherData (arreglo, 2); 
+}
